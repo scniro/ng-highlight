22f247b46e9d5a0e03c2f76c5dbcffe78ef90613
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-21 13:59:59 -0600

    project layout

--- src/ng-highlight.js
+++ src/ng-highlight.js
@@ -1,1 +1,26 @@
-﻿// init
+﻿angular.module('ngHighlight', []).directive('highlight', [function () {
+    return {
+        restrict: 'A',
+        scope: {
+            'highlight': '='
+        },
+        link: function (scope, elem, attrs) {
+
+            scope.$watch('highlight', function (newVal, oldVal) {
+
+                if (newVal && (newVal !== oldVal)) {
+                    var r = RegExp('(' + newVal + ')', 'gi');
+                    var match = elem.text().match(r);
+
+                    if (match) {
+                        var node = elem.text().replace(r, '<span class="highlight">$1</span>');
+                        elem.html(node);
+                    } else {
+                        elem.html('<span>' + elem.text() + '</span>');
+                    }
+                } else
+                    elem.html('<span>' + elem.text() + '</span>');
+            });
+        }
+    }
+}]);
