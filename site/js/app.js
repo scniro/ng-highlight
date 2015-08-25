44d355209d0701bc0c518bcc50872ffa99c6b0f8
Salvatore Niro <salvatore.niro@outlook.com>
2015-08-25 14:24:55 -0600

    init basic site

---
+++ site/js/app.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('app', ['ngHighlight']);
+
+app.controller('ctrl', function ($scope) {
+    $scope.data = [
+        { 'name': 'bob', 'age': 21, 'ocupation': 'skater' },
+        { 'name': 'ethan', 'age': 23, 'ocupation': 'EMT' },
+        { 'name': 'diana', 'age': 36, 'ocupation': 'server' },
+        { 'name': 'miiiiiike', 'age': 18, 'ocupation': 'gamer' },
+        { 'name': 'craignasty', 'age': 21, 'ocupation': 'designer' },
+        { 'name': 'riggins', 'age': 23, 'ocupation': 'dudebro' }
+    ];
+
+    $scope.data2 = [
+        { 'name': 'bob', 'age': 21, 'ocupation': 'skater' },
+        { 'name': 'ethan', 'age': 23, 'ocupation': 'EMT' },
+        { 'name': 'diana', 'age': 36, 'ocupation': 'server' },
+        { 'name': 'miiiiiike', 'age': 18, 'ocupation': 'gamer' },
+        { 'name': 'craignasty', 'age': 21, 'ocupation': 'designer' },
+        { 'name': 'riggins', 'age': 23, 'ocupation': 'dudebro' }
+    ];
+});
