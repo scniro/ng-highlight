5a8a64713052c36709f75663aed7b682d9b4e4bc
Salvatore Niro <salvatore.niro@outlook.com>
2015-09-15 09:39:57 -0600

    init build process

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+﻿/// <binding ProjectOpened='sass:watch' />
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+
+var sass = require('gulp-sass');
+
+var autoprefixer = require('gulp-autoprefixer');
+
+var rename = require('gulp-rename');
+
+var del = require('del');
+
+var util = require('gulp-util');
+
+gulp.task('sass', function () {
+	return gulp.src('site/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('site/css'));
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('site/sass/*.scss', ['sass']);
+});
+
+gulp.task('minify-js', ['clean-js-dist'], function () {
+	return gulp.src('src/*.js')
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean-js-dist', function (done) {
+	var file = 'dist/ng-highlight.min.js';
+	util.log(util.colors.green('cleaning'), ' -- ', util.colors.yellow(file));
+	del(file, done);
+});
+
+
+gulp.task('build', ['minify-js']);
