aedd8b871e29540d10e196c2decad4c0e18539c1
Salvatore Niro <salvatore.niro@outlook.com>
2015-09-15 20:47:01 -0600

    setting up static data

--- src/ng-highlight.js
+++ src/ng-highlight.js
@@ -1,26 +1,43 @@
-﻿angular.module('ngHighlight', []).directive('highlight', [function () {
-    return {
-        restrict: 'A',
-        scope: {
-            'highlight': '='
-        },
-        link: function (scope, elem, attrs) {
+﻿function init() {
 
-            scope.$watch('highlight', function (newVal, oldVal) {
+	angular.module('ngHighlight', []).directive('highlight', [
+		function() {
+			return {
+				restrict: 'A',
+				scope: {
+					'highlight': '='
+				},
+				link: function(scope, elem, attrs) {
 
-                if (newVal && (newVal !== oldVal)) {
-                    var r = RegExp('(' + newVal + ')', 'gi');
-                    var match = elem.text().match(r);
+					console.log('elem');
 
-                    if (match) {
-                        var node = elem.text().replace(r, '<span class="highlight">$1</span>');
-                        elem.html(node);
-                    } else {
-                        elem.html('<span>' + elem.text() + '</span>');
-                    }
-                } else
-                    elem.html('<span>' + elem.text() + '</span>');
-            });
-        }
-    }
-}]);
+					scope.$watch('highlight', function(newVal, oldVal) {
+
+						if (newVal && (newVal !== oldVal)) {
+							var r = RegExp('(' + newVal + ')', 'gi');
+							var match = elem.text().match(r);
+
+							if (match) {
+								var node = elem.text().replace(r, '<span class="highlight">$1</span>');
+								elem.html(node);
+							} else {
+								elem.html('<span>' + elem.text() + '</span>');
+							}
+						} else
+							elem.html('<span>' + elem.text() + '</span>');
+					});
+				}
+			}
+		}
+	]);
+}
+
+(function () {
+	if (typeof define === 'function' && define.amd) { // RequireJS aware
+		define(['angular'], function () {
+			init();
+		});
+	} else {
+		init();
+	}
+}());
