31daf46ca4da7cc05eb1cb76fd2369554d986e1d
Sal Niro <salvatore.niro@outlook.com>
2015-12-05 09:08:07 -0700

    ui changes/including icon

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,18 @@ var autoprefixer = require('gulp-autoprefixer');
 
 var rename = require('gulp-rename');
 
+var minifyCss = require('gulp-minify-css');
+
 gulp.task('sass', function () {
     return gulp.src('demo-site/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
+        }))
+        .pipe(minifyCss())
+        .pipe(rename({
+            suffix: '.min'
         }))
         .pipe(gulp.dest('demo-site/css'));
 });
